ccc07b10e5b420148fae1d2f3c31480a6f692fa4
super <super@DESKTOP-10MPALR>
2023-12-08 15:59:06 -0500

    Initital Commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        title: 'Control Minecraft with a Bike',
+        imgUrl: '/assets/minecraftbike.jpg',
+        stack: ['GPC', 'Titan Two'],
+        link: '',
+    },
+
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Job Searching',
+        duration: 'Now',
+        details: "With my knowledge of JavaScript, TypeScript and React, I am now looking for open positions at companies that could use my brain, creativity, and skills using this stack!",
+    },
+    {
+        year: '2023',
+        title: 'Solo Web Developer',
+        duration: '1 year',
+        details: "Using React + JS/TS to make websites and web-apps of tools and games I wanted to use, but didn't quite exist",
+    },
+    {   
+        year: "2022",
+        title: "To The Web!",
+        duration: "1.5 years",
+        details: "I've always wanted to make websites, but I never really understood JS or HTML. I would experiment with 'Inspect Element' while on certain websites, but I didn't understand what all the different divs and headers and bodies meant. But I began learning JavaScript!"
+    },
+    {   
+        year: "2022",
+        title: "Graduating High School",
+        duration: "2 years",
+        details: "Upon graduation, I began learning C# and getting the basics down of more practical languages as compared to GML or GPC"
+    },
+    {
+        year: "2020",
+        title: "Getting Serious",
+        duration: "4 years",
+        details: "As most teenagers in 2020, I was playing video games all day, but I also was into exercising. I wondered if there was a way to play Minecraft but by moving in real life. That led me to find a piece of hardware called the Titan Two and a language called GPC, where I developed code to take motion control inputs of jumping, walking, crouching to output the neccesary movement commands, as well as squeezing a resistance ring to attack enemies. It was at this point that I knew I could pursue this as a career."
+    },
+    {
+        year: "2019",
+        title: "First Line of Real Code",
+        duration: 'Rebirth',
+        details: "I was gifted GameMaker Studio 2 and began learning GML (GameMaker Language) and was amazed that programming was more than just dragging blocks in certain ways, but that you could actually write down words and syntax and it would function. I created a few personal projects but nothing amazing. Although in 2020 I designed a chemistry game for my final project in chemistry."
+    },
+    {
+        year: "2013",
+        title: 'First Project Created',
+        duration: 'The Nexus Event',
+        details: 'Using the GameMaker 7 program with drag-and-drop progamming, I began to explore the fundementals of coding with variables, loops, and if statements. At age 9, I believe this set me down the path to pursuing software development.'
+    }
+]
